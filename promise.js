a32df28ee0d9ab784ac275ff2c5a4345c5ae01de
muhamadjaya <jayamuhamad99@gmail.com>
2022-09-17 23:34:08 +0700

    menambahkan promise pada file promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const theaterIxx = await promiseTheaterIXX();
+    const theaterVgc = await promiseTheaterVGC();
+    const allTheater = [...theaterIxx, ...theaterVgc];
+
+    const emotions = allTheater.filter((emotion) => emotion.hasil == emosi);
+
+    return emotions.length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
